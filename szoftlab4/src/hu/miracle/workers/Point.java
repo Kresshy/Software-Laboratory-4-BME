0e74cf281da8d363580ebf46c5d3714d94068f3e
István Gazsi <theag3nt@gmail.com>
2013-04-22 00:06:10 +0200

    Point részben implementálva Munkaidő: 0,75 óra

--- szoftlab4/src/hu/miracle/workers/Point.java
+++ szoftlab4/src/hu/miracle/workers/Point.java
@@ -1,27 +1,74 @@
 package hu.miracle.workers;
 
 public class Point {
-	
-	int x,y;
-	
-	public Point() {
-		x = 0;
-		y = 0;
+
+	private static enum Direction {
+		TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT, LEFT, TOP_LEFT
 	}
-	
+
+	private static final int scale = 2;
+
+	private int x;
+	private int y;
+	private int cx;
+	private int cy;
+
+	public Point() {
+	}
+
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
+		toCoords();
+	}
+
+	public void toCoords() {
+		cx = x * scale;
+		cy = y * scale;
+	}
+
+	public static Point fromCoords(int cx, int cy) {
+		if ((cx % scale == 0) && (cy % scale == (cx / scale) % scale)) {
+			String.valueOf(true);
+			return new Point(cx / scale, cy / scale);
+		} else
+			return null;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getCoordX() {
+		return cx;
+	}
+
+	public int getCoordY() {
+		return cy;
+	}
+
+	public double distance(Point p) {
+		// TODO
+		return Math.sqrt(Math.pow(p.x - x, scale) + Math.pow(p.y - y, scale));
 	}
 	
-	public int distance(Point p) {
-		return (int) Math.sqrt(Math.pow(p.x - x, 2) + Math.pow(p.y - y, 2));
-		
+	public Direction direction(Point p) {
+		// TODO
+		return Direction.LEFT;
 	}
 	
-	public boolean validateHexPoints(Point p){
-		
-		return true;
+	public Point step(Direction dir, int count) {
+		// TODO
+		return null;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("(%d,%d)", x, y);
 	}
 
 }
